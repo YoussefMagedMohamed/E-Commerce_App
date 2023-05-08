4cdc0616a680ea3b153062d218d68a2e91c1b8b2
YoussefMagedMohamed <joeyoussef307@gmail.com>
2023-05-08 20:18:17 +0300

    Routring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 const App = () => {
   return (
@@ -10,7 +12,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact/>
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
